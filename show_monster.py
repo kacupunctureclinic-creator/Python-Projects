f76719696737102a4005e3d37e5137c3740505e1
kacupunctureclinic-creator <kacupunctureclinic@gmail.com>
2026-09-09 21:43:36 +0100

    Monster
    Nice or Mean_2

---
+++ show_monster.py
@@ -0,0 +1,11 @@
+def show_monster():
+    # 여러 줄의 문자열을 표현할 때는 앞서 배운 """(독스트링 형태)를 활용하면 좋습니다!
+    monster_image = """
+       /\_/\  
+      ( o.o ) 
+       > ^ <
+      무서운 고양이 몬스터가 나타났다!
+    """
+    print(monster_image)
+
+show_monster()
